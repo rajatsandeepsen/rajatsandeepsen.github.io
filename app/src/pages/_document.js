2422bfee87c73570020a6fb4578539cbe1ab4f42
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-03-02 10:40:30 +0000

    added electron and fixed document link href

--- app/src/pages/_document.js
+++ app/src/pages/_document.js
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <title>Rajat Sandeep</title>
+      <meta name="description" content="Redirection Page" />
+      <meta property="og:image" content="https://github.com/rajatsandeepsen/rajatsandeepsen/blob/main/main.png?raw=true" />
+      <meta property="og:title" content="Rajat Sandeep" />
+      <meta property="og:description" content="Rajat Sandeep's Landing Page" />
+      <meta property="og:url" content="https://rajatsandeepsen.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Rajat Sandeep" />
+      <meta property="og:locale" content="en_US" />
+      <meta name="keywords" content="Rajat Sandeep, Rajat, Rajat Sandeep Sen, sjcet, sjcetpalai, precepter, presept" />
+      <meta name="author" content="Rajat Sandeep" />
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="google" content="nositelinkssearchbox" />
+      <meta name="google" content="notranslate" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="short icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"></link>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+
+      </Head>
       <body>
         <Main />
         <NextScript />
