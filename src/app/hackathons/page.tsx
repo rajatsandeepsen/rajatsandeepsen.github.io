7526d1cf1a9dd6a73752d45de64e4810c2b7fe1d
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2026-04-27 19:58:47 +0530

    fix: text

--- src/app/hackathons/page.tsx
+++ src/app/hackathons/page.tsx
@@ -13,12 +13,14 @@ export default function Page() {
 						<div className="flex flex-col items-center justify-center space-y-4 text-center">
 							<div className="space-y-2">
 								<div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-									Hackathons & other "W" s
+									Hackathons
 								</div>
 								<h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
 									HAAAAAACK
 								</h2>
-								<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed"></p>
+								<p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+									Some of my small wins
+								</p>
 							</div>
 						</div>
 					</BlurFade>
